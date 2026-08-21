9d8ea43bffe56df3f2cecb22f26606752c8b74ae
Sabari Bala M R <sabaribala7733@gmail.com>
2026-08-21 21:06:29 +0530

    Add deployment configuration

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,19 +1,19 @@
-import os
+﻿import os
+import dj_database_url
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend', 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
